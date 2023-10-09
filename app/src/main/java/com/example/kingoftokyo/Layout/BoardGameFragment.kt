a6d4e10d78bbf8cc3d09e0c38eb42ee62ebaed34
Mdetorquat <maxence.de-torquat-de-la-coulerie@epita.fr>
2023-10-09 11:56:41 +0200

    commit v2

--- app/src/main/java/com/example/kingoftokyo/Layout/BoardGameFragment.kt
+++ app/src/main/java/com/example/kingoftokyo/Layout/BoardGameFragment.kt
@@ -12,11 +12,6 @@ import com.example.kingoftokyo.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BoardGameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BoardGameFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
