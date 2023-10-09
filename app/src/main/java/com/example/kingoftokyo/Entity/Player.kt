d0920f9ed3f407cefc81c7a39b8f024d81af0cf2
capinho <papadiawara@hotmail.fr>
2023-10-09 10:42:05 +0200

    add Market model and Game viewModel

--- app/src/main/java/com/example/kingoftokyo/Entity/Player.kt
+++ app/src/main/java/com/example/kingoftokyo/Entity/Player.kt
@@ -12,72 +12,39 @@ open class Player(
     private val cards: MutableList<Card> = mutableListOf(),
     val imageResId: Int = R.drawable.default_image
 ) {
+    val isAlive: Boolean get() = health > 0
 
-    val isAlive: Boolean
-        get() = health > 0
-
-    fun useCard(card: Card) {
-        if (card !in cards) {
-            println("You don't have this card.")
-            return
-        }
-
-        if (energy >= card.energyCost) {
-            card.performAction(this)
-            energy -= card.energyCost
-        } else {
-            println("Not enough energy to use this card.")
-        }
+    fun useCard(card: Card): Boolean {
+        if (card !in cards || energy < card.energyCost) return false
+        card.performAction(this)
+        energy -= card.energyCost
+        return true
     }
 
-    fun addCard(card: Card) {
-        if (card in cards) {
-            println("You already have this card.")
-            return
-        }
-
+    fun addCard(card: Card): Boolean {
+        if (card in cards) return false
         cards.add(card)
+        return true
     }
 
-    fun removeCard(card: Card) {
-        if (card !in cards) {
-            println("You don't have this card.")
-            return
-        }
-
+    fun removeCard(card: Card): Boolean {
+        if (card !in cards) return false
         cards.remove(card)
+        return true
     }
 
     fun takeDamage(damage: Int) {
-        if (damage < 0) {
-            println("Invalid damage amount.")
-            return
-        }
-
-        health -= damage
-        if (health < 0) health = 0
+        if (damage < 0) return
+        health = (health - damage).coerceAtLeast(0)
     }
 
     fun heal(amount: Int) {
-        if (amount < 0) {
-            println("Invalid healing amount.")
-            return
-        }
-
-        if (isInTokyo) {
-            println("Cannot heal while in Tokyo!")
-            return
-        }
-
-        health += amount
-        if (health > 10) health = 10
+        if (amount < 0 || isInTokyo) return
+        health = (health + amount).coerceAtMost(10)
     }
 
     fun addVictoryPoints(points: Int) {
-        if (points < 0) {
-            println("Invalid points.")
-            return
-        }
+        if (points < 0) return
         victoryPoints += points
     }
 
@@ -95,43 +62,38 @@ open class Player(
         }
     }
 
-    fun attack(opponents: List<Player>, damage: Int, tokyo: Tokyo) {
-        if (damage < 0) {
-            println("Invalid damage amount.")
-            return
-        }
+    open fun decisionToLeaveTokyo(): Boolean = false
+
+    fun attack(opponents: List<Player>, damage: Int, tokyo: Tokyo): List<Player> {
+        if (damage < 0) return emptyList()
+
+        val eliminatedPlayers = mutableListOf<Player>()
 
         if (isInTokyo) {
             for (opponent in opponents) {
                 if (!opponent.isInTokyo) {
                     opponent.takeDamage(damage)
+                    if (!opponent.isAlive) {
+                        eliminatedPlayers.add(opponent)
+                    }
                 }
             }
         } else {
             tokyo.currentPlayerInTokyo?.takeDamage(damage)
-
             if (tokyo.currentPlayerInTokyo?.isAlive == false || tokyo.currentPlayerInTokyo?.decisionToLeaveTokyo() == true) {
                 tokyo.leaveTokyo(tokyo.currentPlayerInTokyo!!)
                 enterTokyo(tokyo)
             }
         }
-    }
 
-    open fun decisionToLeaveTokyo(): Boolean {
-        println("$name, you've been attacked in Tokyo. Do you want to leave Tokyo? (yes/no)")
-        val answer = readLine()
-        return answer?.toLowerCase() == "yes"
+        return eliminatedPlayers
     }
 
 }
 
 class IAPlayer(name: String) : Player(name, imageResId = R.drawable.ia_image, isHuman = false) {
     override fun decisionToLeaveTokyo(): Boolean {
-        //
-        if (health < 5) {
-            println("$name has decided to leave Tokyo.")
-            return true
-        }
-        return false
+        // Leave Tokyo if health is below 5 or if AI has more than 15 victory points to protect its lead
+        return health < 5 || victoryPoints > 15
     }
 }
