e96cb9a6aa0c2233b2093005f57acaaab489bf39
gregglrnt <gregoryslaurent@gmail.com>
2023-10-11 12:23:21 +0200

    update

--- app/src/main/java/com/example/kingoftokyo/Layout/MenuFragment.kt
+++ app/src/main/java/com/example/kingoftokyo/Layout/MenuFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.kingoftokyo.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -38,23 +40,11 @@ class MenuFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val playButton : Button = view.findViewById(R.id.playButton)
+        playButton.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_boardGameFragment)
+        }
     }
+
 }
